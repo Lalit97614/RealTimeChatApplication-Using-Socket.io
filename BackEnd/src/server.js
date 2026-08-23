461c31ec358943ee2205e047bdfd676b19aa081c
Lalit97614 <lalitrajpoot976148@gmail.com>
2026-08-23 18:10:25 +0530

    Add production build and frontend serving

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -2,14 +2,40 @@ import express from "express"
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
-
-dotenv.config();
-const PORT = process.env.PORT||5000;
+import { fileURLToPath } from "url";
+import path from "path"
 
 const app=express();
+dotenv.config();
+
+
+
+const PORT = process.env.PORT||5000;
+const __filename=fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+console.log(__dirname)
+console.log(__filename)
+
+
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
+
+//Ready to Production ....
+if(process.env.NODE_ENV==="production"){
+     const frontendPath = path.join(
+        __dirname,
+        "../../FrontEnd/dist"
+    );
+    console.log("FrontEndPath--->" ,frontendPath)
+
+    app.use(express.static(frontendPath))
+
+    app.use("*",(req,resp)=>{
+      resp.sendFile(path.join(frontendPath,"index.html"))
+    })
+}
+
 
 
 app.listen(PORT,()=>{
