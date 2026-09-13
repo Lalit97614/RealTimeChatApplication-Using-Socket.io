571a1017c8f4244440cd151e49619051232a5265
Lalit97614 <lalitrajpoot976148@gmail.com>
2026-09-13 11:30:35 +0530

    Add logout and login functionality

--- BackEnd/src/controllers/auth.controller.js
+++ BackEnd/src/controllers/auth.controller.js
@@ -46,14 +46,14 @@ export const signup = async (req, resp) => {
 
 
 
-        const hashPassword=await bcrypt.hash(password,10);
+        const hashPassword = await bcrypt.hash(password, 10);
 
-        const user = await User.create({fullname,email,password:hashPassword});
+        const user = await User.create({ fullname, email, password: hashPassword });
 
-        const token = generateToken(user._id,resp);
+        const token = generateToken(user._id, resp);
 
 
-       // Send welcome email
+        // Send welcome email
         try {
             await sendWelcomeEmail(
                 user.email,
@@ -65,22 +65,74 @@ export const signup = async (req, resp) => {
         }
 
         return resp.status(201).json({
-            success:true,
-            message:"User is successfully registered",
-            user:{
-                id:user._id,
-               email:user.email,
-               fullname :user.fullname
+            success: true,
+            message: "User is successfully registered",
+            user: {
+                id: user._id,
+                email: user.email,
+                fullname: user.fullname
             }
         })
 
-        
 
-    } catch (error) {   
-    console.log(error);
-    resp.status(500).json({
-        success:false ,
-        message:"Internal Server Error"
-    })
+
+    } catch (error) {
+        console.log(error);
+        resp.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
     }
+}
+export const login = async (req, resp) => {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+            return resp.status(400).json({
+                success: false,
+                message: "Email and password are required"
+            });
+        }
+        const user = await User.findOne({ email: email })
+        if (!user) {
+            return resp.status(400).json({
+                success: false,
+                message: "Invalid credentials"
+            })
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return resp.status(400).json({
+                success: false,
+                message: "Invalid credentials"
+            })
+        }
+        const token = generateToken(user._id, resp);
+        return resp.status(200).json({
+            success: true,
+            message: "User is successfully LogedIn",
+            user: {
+                id: user._id,
+                fullname: user.fullname,
+                email: user.email,
+                profilePic: user.profilePic
+            }
+        })
+
+
+    } catch (error) {
+        console.error("Error in Login Controller", error);
+        resp.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+}
+
+export const logout = async (_, resp) => {
+    resp.cookie("jwt", "", { maxAge: 0 });
+    return resp.status(200).json({
+        success: true,
+        message: "Logged out successfully"
+    })
 }
